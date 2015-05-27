27418254562300d9ee8011f174ab05b8a30b5d95
cepnyci <aaytekin@gmail.com>
2015-05-27 18:04:28 +0300

    Clustering added.

--- src/main/java/com/zaana/ml/tests/ClusterTests.java
+++ src/main/java/com/zaana/ml/tests/ClusterTests.java
@@ -8,8 +8,28 @@ import com.zaana.ml.similarity.Cosine;
  */
 public class ClusterTests extends AbstractTests{
 
+    /*String[] names = new String[] { "O1", "O2", "O3", "O4", "O5", "O6" };
+    double[][] distances = new double[][] {
+            { 0, 1, 9, 7, 11, 14 },
+            { 1, 0, 4, 3, 8, 10 },
+            { 9, 4, 0, 9, 2, 8 },
+            { 7, 3, 9, 0, 6, 13 },
+            { 11, 8, 2, 6, 0, 10 },
+            { 14, 10, 8, 13, 10, 0 }};
+
+    ClusteringAlgorithm alg = new DefaultClusteringAlgorithm();
+    Cluster cluster = alg.performClustering(distances, names,
+            new AverageLinkageStrategy());
+    Alternatively, you can pass a pdist-like matrix containing one row:
+
+    String[] names = new String[] { "O1", "O2", "O3", "O4", "O5", "O6" };
+    double[][] pdist = new double[][] {
+            {1, 9, 7, 11 ,14 ,4 ,3 ,8 ,10 ,9 ,2 ,8 ,6 ,13 ,10}
+    };
+    ClusteringAlgorithm alg = new PDistClusteringAlgorithm();
+    Cluster cluster = alg.performClustering(pdist, names, new AverageLinkageStrategy());*/
     public static void runClusterTests(String dataFileBase, String seperator, int y) {
         preprocessDataForValidation(dataFileBase, 1, "val", seperator);
-        Cosine.createDistanceMatrix(userRateMap,y);
+        Cosine.createDistanceMatrix(userRateMap, y);
     }
 }
